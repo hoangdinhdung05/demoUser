3d53086d9daac92300954a1559cf9ff4d4bc7381
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2025-07-31 08:05:03 +0700

    create project demo user

---
+++ src/test/java/com/training_java/demo_user/DemoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training_java.demo_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
